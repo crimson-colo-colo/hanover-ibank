c8af01e9aa44918e2549d1698909738f5be6af46
Caleb Chan <cchan@hey.com>
2026-04-01 22:51:58 -0400

    feat: add seed data to script

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,13 @@
 import { PrismaPg } from "@prisma/adapter-pg"
-import { PrismaClient } from "../server/generated/prisma/client.ts"
+import {
+	ContentStatus,
+	ContentType,
+	DocumentType,
+	EmployeeRole,
+	Prisma,
+	PrismaClient
+} from "../server/generated/prisma/client.ts"
+import crypto from "node:crypto"
 
 const adapter = new PrismaPg({
 	connectionString: process.env.DATABASE_URL!,
@@ -19,10 +27,10 @@ main()
 async function main() {
 	await prisma.$connect()
 
-	const [users, content] = await Promise.all([prisma.user.count(), prisma.content.count()])
-	if (users > 0 || content > 0) {
+	const [employees, content] = await Promise.all([prisma.employee.count(), prisma.content.count()])
+	if (employees > 0 || content > 0) {
 		process.stdout.write(
-			`⚠️ \x1b[33mDatabase already has data (users: ${users}, content: ${content}. Continuing will erase existing data and cannot be undone. Really continue[y/N] \x1b[0m`
+			`⚠️ \x1b[33mDatabase already has data (employee: ${employees}, content: ${content}. Continuing will erase existing data and cannot be undone. Really continue[y/N] \x1b[0m`
 		)
 		const answer = await new Promise<string>((resolve) => {
 			process.stdin.setEncoding("utf-8")
@@ -40,5 +48,119 @@ async function main() {
 
 	console.log("🌱 Seeding database...")
 
-	await prisma.$transaction([prisma.user.deleteMany(), prisma.content.deleteMany()])
+	await prisma.$transaction([prisma.employee.deleteMany(), prisma.content.deleteMany()])
+
+	// Wilson Harper, wharper@hanover.com, Business Analyst
+	// Austin Johnson, ajohnson@hanover.com, Underwriter
+	// Jack Needleham, jneedleham@hanover.com, Business Analyst
+	// Sarah Miles, smiles@hanover.com, Underwriter
+	// Emily Cane, ecane@hanover.com,
+
+	const employeeData = [
+		{
+			name: "Wilson Harper",
+			email: "wharper@hanover.com",
+			role: EmployeeRole.BusinessAnalyst,
+		},
+		{
+			name: "Austin Johnson",
+			email: "ajohnson@hanover.com",
+			role: EmployeeRole.Underwriter,
+		},
+		{
+			name: "Jack Needleham",
+			email: "jneedleham@hanover.com",
+			role: EmployeeRole.BusinessAnalyst,
+		},
+		{
+			name: "Sarah Miles",
+			email: "smiles@hanover.com",
+			role: EmployeeRole.Underwriter,
+		},
+		{
+			name: "Emily Cane",
+			email: "ecane@hanover.com",
+			role: EmployeeRole.BusinessAnalyst,
+		}
+	]
+
+	const [wilson, austin, jack, sarah, emily] = await prisma.$transaction(
+		employeeData.map((employee) => (prisma.employee.create({
+			data: {
+				...employee,
+				avatarUrl: getAvatarUrl(employee.email),
+			},
+			select: { id: true }
+		})))
+	)
+
+    console.log(`Created ${employeeData.length} employee rows`)
+
+	const contentData = [
+		{
+			title: "Risk Meter",
+			description: "",
+			type: ContentType.Link,
+			url: "https://riskmeter.corelogic.com",
+			ownerId: sarah.id,
+			lastModifiedDate: new Date("2026-03-27"),
+			expirationDate: new Date("2027-01-01"),
+			documentType: DocumentType.Workflow,
+			status: ContentStatus.Complete,
+		},
+		{
+			title: "Kentucky Tax Law",
+			description: "",
+			type: ContentType.Link,
+			url: "https://revenue.ky.gov/Get-Help/pages/research-tax-laws.aspx",
+			ownerId: wilson.id,
+			lastModifiedDate: new Date("2026-02-04"),
+			expirationDate: new Date("2026-04-15"),
+			documentType: DocumentType.Reference,
+			status: ContentStatus.UnderReview,
+		},
+		{
+			title: "Image Editor",
+			description: "",
+			type: ContentType.Link,
+			url: "https://www.adobe.com/express/feature/image/editor",
+			ownerId: emily.id,
+			lastModifiedDate: new Date("2025-10-26"),
+			expirationDate: new Date("2027-01-01"),
+			documentType: DocumentType.Workflow,
+			status: ContentStatus.Complete,
+		},
+		{
+			title: "Workstation",
+			description: "",
+			type: ContentType.Link,
+			url: "https://drive.google.com/drive/my-drive",
+			ownerId: austin.id,
+			lastModifiedDate: new Date("2025-07-13"),
+			expirationDate: new Date("2026-06-15"),
+			documentType: DocumentType.Workflow,
+			status: ContentStatus.Incomplete,
+		},
+		{
+			title: "Oregon Tax Law",
+			description: "",
+			type: ContentType.Link,
+			url: "https://www.oregon.gov/dor/pages/rules-laws.aspx",
+			ownerId: jack.id,
+			lastModifiedDate: new Date("2025-12-12"),
+			expirationDate: new Date("2026-04-15"),
+			documentType: DocumentType.Reference,
+			status: ContentStatus.Complete,
+		}
+	] satisfies Prisma.ContentCreateManyInput[]
+    
+    await prisma.content.createMany({ data: contentData })
+    
+    console.log(`Created ${contentData.length} content rows`)
+}
+
+// returns the gravtar url for the given email
+function getAvatarUrl(email: string) {
+	const hash = crypto.createHash("md5").update(email.trim().toLowerCase()).digest("hex")
+	return `https://www.gravatar.com/avatar/${hash}?d=identicon`
 }
