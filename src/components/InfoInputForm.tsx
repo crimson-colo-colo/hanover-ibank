cb4bca20d427e926dbbb69b807a602263ba2e297
Justin Fletcher <justin.fletcher22@gmail.com>
2026-04-02 21:57:22 -0400

    feat: add dropdown for document status

--- src/components/InfoInputForm.tsx
+++ src/components/InfoInputForm.tsx
@@ -8,6 +8,7 @@ export function InfoInputForm() {
 				label="What is Your Role?"
 				placeholder="Pick One"
 				data={["Underwriter", "Business Analyst"]}
+				clearable
 			/>
 
 			<TextInput mt="sm" label="First Name" placeholder="Input Name" />
@@ -15,6 +16,14 @@ export function InfoInputForm() {
 			<TextInput mt="sm" label="Input Name" placeholder="Input Name" />
 
 			<TextInput mt="sm" label="Input Email Adress" placeholder="Input Email Adress" />
+
+			<Select
+				mt="sm"
+				label="What is the status of document you are uploading?"
+				placeholder="Select One"
+				data={["Complete", "Incomplete", "Under Review"]}
+				clearable
+			/>
 
 			<Button mt="sm" variant="filled">
 				Submit
