4c1f8963e80586a34405148c5370943c0a94d1d1
a1cd <71281043+a1cd@users.noreply.github.com>
2026-04-02 21:55:26 -0400

    Add DatePicker to InfoInputForm and install @mantine/dates and dayjs.

--- src/components/InfoInputForm.tsx
+++ src/components/InfoInputForm.tsx
@@ -1,22 +1,25 @@
 import { Button, Select, TextInput } from "@mantine/core"
+import { DatePickerInput } from "@mantine/dates"
 
 export function InfoInputForm() {
 	return (
 		<form className="max-w-md mx-auto">
 			<Select
 				mt="sm"
-				label="What is Your Role?"
+				label="Your Role"
 				placeholder="Pick One"
 				data={["Underwriter", "Business Analyst"]}
 			/>
 
-			<TextInput mt="sm" label="First Name" placeholder="Input Name" />
+			<TextInput mt="sm" label="First Name" placeholder="Input First Name" />
 
-			<TextInput mt="sm" label="Input Name" placeholder="Input Name" />
+			<TextInput mt="sm" label="Last Name" placeholder="Input Last Name" />
 
-			<TextInput mt="sm" label="Input Email Adress" placeholder="Input Email Adress" />
+			<TextInput mt="sm" label="Email Adress" placeholder="Input Email Adress" />
 
-			<Button mt="sm" variant="filled">
+			<DatePickerInput mt="sm" label="Deadline" placeholder="Pick date" />
+
+			<Button mt="md" variant="filled">
 				Submit
 			</Button>
 		</form>
