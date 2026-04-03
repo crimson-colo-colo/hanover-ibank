1624a8259ed9b537c819384568400e0b637554f0
Caleb Chan <cchan@hey.com>
2026-04-02 22:50:18 -0400

    style: change button color

--- src/components/scroll-to-top-button.tsx
+++ src/components/scroll-to-top-button.tsx
@@ -30,7 +30,7 @@ export function ScrollToTopButton(
                     aria-label="Scroll to top"
                     rightSection={<IconArrowUp size={16} />}
                     variant="filled"
-                    color="#15aabf"
+                    color="teal"
                     radius="x1"
                     style={{
                         ...styles,
