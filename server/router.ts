17f84bc4eb6763d5070e4ed5590e86484348c31d
a1cd <71281043+a1cd@users.noreply.github.com>
2026-04-07 13:23:29 -0400

    remove unused import
    Signed-off-by: a1cd <71281043+a1cd@users.noreply.github.com>

--- server/router.ts
+++ server/router.ts
@@ -1,7 +1,7 @@
 import { cliRouter } from "./routers/cli.ts"
 import { contentRouter } from "./routers/content.ts"
 import { storageRouter } from "./s3.ts"
-import { authProcedure, router } from "./trpc.ts"
+import { router } from "./trpc.ts"
 
 export type AppRouter = typeof appRouter
 export const appRouter = router({
