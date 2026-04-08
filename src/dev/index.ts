ed6bd3d6d72bf859f7d336e719065678cacedd27
Caleb Chan <cchan@hey.com>
2026-04-07 22:04:46 -0400

    fix: remove react buddy stuff

--- src/dev/index.ts
+++
@@ -1,6 +0,0 @@
-import React from "react"
-import { useInitial } from "./useInitial.ts"
-
-const ComponentPreviews = React.lazy(() => import("./previews.tsx"))
-
-export { ComponentPreviews, useInitial }
