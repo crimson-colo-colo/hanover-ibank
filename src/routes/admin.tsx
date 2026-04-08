66eedf17a74b12d5906c0bb697b29add156f202a
Caleb Chan <cchan@hey.com>
2026-04-08 11:14:45 -0400

    fix: redirect to / on failed query

--- src/routes/admin.tsx
+++ src/routes/admin.tsx
@@ -4,8 +4,14 @@ import { trpcClient } from "@/lib/trpc.ts"
 export const Route = createFileRoute("/admin")({
 	component: RouteComponent,
 	beforeLoad: async (ctx) => {
-		const isAdmin = await trpcClient.admin.isAdmin.query()
-		if (!isAdmin) {
+		try {
+			const isAdmin = await trpcClient.admin.isAdmin.query()
+			if (!isAdmin) {
+				throw redirect({
+					to: "/",
+				})
+			}
+		} catch {
 			throw redirect({
 				to: "/",
 			})
