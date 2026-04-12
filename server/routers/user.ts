e039df74a1721865d0f80d09c5b4ada70e7e431d
Justin Fletcher <justin.fletcher22@gmail.com>
2026-04-11 20:51:47 -0400

    feat: add /profile supporting procedures
    Signed-off-by: Caleb Chan <cchan@hey.com>

---
+++ server/routers/user.ts
@@ -0,0 +1,67 @@
+import sharp from "sharp"
+import z from "zod"
+import { auth0Management } from "../auth.ts"
+import { db } from "../database.ts"
+import { bucketName, s3 } from "../s3.ts"
+import { authProcedure, router } from "../trpc.ts"
+
+export const userRouter = router({
+	getProfile: authProcedure.query(async (opts) => {
+		const userRole = await db.employee.findUnique({
+			where: {
+				id: opts.ctx.auth.sub,
+			},
+			select: {
+				role: true,
+			},
+		})
+		const auth0User = await auth0Management.users.get(opts.ctx.auth.sub)
+		return {
+			id: opts.ctx.auth.sub,
+			name: auth0User.name ?? auth0User.nickname ?? auth0User.username!,
+			email: auth0User.email!,
+			username: auth0User.username!,
+			role: userRole?.role,
+		}
+	}),
+	updateProfile: authProcedure
+		.input(
+			z.object({
+				name: z.string().min(3).max(100),
+				email: z.email(),
+				username: z.string().min(3).max(100),
+			})
+		)
+		.mutation(async (opts) => {
+			try {
+				await auth0Management.users.update(opts.ctx.auth.sub, {
+					name: opts.input.name,
+					email: opts.input.email,
+					username: opts.input.username,
+				})
+				return "Successfully updated user profile information"
+			} catch {
+				return "Failed to update user profile information (duplicate email or username)."
+			}
+		}),
+	uploadAvatar: authProcedure
+		.input(
+			z.object({
+				file: z.base64().max(10_000_000 * 1.3),
+			})
+		)
+		.mutation(async (opts) => {
+			const img = sharp(Buffer.from(opts.input.file, "base64")).resize({
+				width: 300,
+				height: 300,
+				withoutEnlargement: true,
+			})
+
+			const buffer = await img.png().toBuffer()
+			await s3.putObject({
+				Bucket: bucketName,
+				Key: `avatar/${opts.ctx.auth.sub}.png`,
+				Body: buffer,
+			})
+		}),
+})
