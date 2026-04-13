8eac7e74437facb1cc565d3cf280ff499c6d98a0
PhilBanoub <pbanoub@wpi.edu>
2026-04-13 18:01:43 -0400

    added procedure signatures

--- server/routers/forms.ts
+++ server/routers/forms.ts
@@ -1,3 +1,4 @@
+import { useMutation } from "@tanstack/react-query"
 import { v4 as uuidv4 } from "uuid"
 import z from "zod"
 import { auth0Management } from "../auth.ts"
@@ -12,6 +13,7 @@ import { getFileTypeFromFile } from "../lib/filetype.ts"
 import { getGravatarUrl, isoDateToTimestamp } from "../lib.ts"
 import { bucketName, s3 } from "../s3.ts"
 import { authProcedure, publicProcedure, router } from "../trpc.ts"
+import { userRouter } from "./user.ts"
 
 const baseSchema = z.object({
 	name: z.string().max(250).min(3),
@@ -101,4 +103,7 @@ export const formsRouter = router({
 				}
 			})
 		}),
+
+	checkIn: publicProcedure.input(z.object({ id: z.string() })).mutation(async (opts) => {}),
+	checkOut: publicProcedure.input(z.object({ id: z.string() })).mutation(async (opts) => {}),
 })
