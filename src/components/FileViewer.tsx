472e7ba6fe86fbbaf90010f959883c4ccf831953
lucasbassem <lucaszaki2@gmail.com>
2026-04-14 00:05:28 -0400

    Changes by lukeb
    Signed-off-by: a1cd <71281043+a1cd@users.noreply.github.com>

--- src/components/FileViewer.tsx
+++ src/components/FileViewer.tsx
@@ -1,5 +1,35 @@
+import { Stack, Text, Title } from "@mantine/core"
 import type { ContentListItem } from "../../server/routers/content.ts"
+import {useQuery} from "@tanstack/react-query";
+import {trpc} from "@/lib/trpc.ts";
+import DocViewer, { DocViewerRenderers} from "@iamjariwala/react-doc-viewer"
+import "@iamjariwala/react-doc-viewer/dist/index.css"
 
 export function FileViewer({ content }: { content: ContentListItem }) {
-	return <p></p>
+	const fileURL = useQuery(trpc.content.download.queryOptions({ id: content.id }))
+	// https://react-pdf.org/components#pdfviewer
+	return (
+
+		<Stack gap="sm">
+			<Title order={3}>{content.title} </Title>
+
+			<Text>
+				<b>ID:</b> {content.id}
+			</Text>
+
+			<Text>
+				<b>Type:</b>
+				{content.type}
+			</Text>
+			<Text>
+				<b>Owner: </b>
+				{content.owner.name}
+			</Text>
+			<Text>
+				<b>Status: </b>
+				{content.status}
+			</Text>
+		</Stack>
+
+	)
 }
