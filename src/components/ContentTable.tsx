8866322e4ec559e4d631f97a0628389acc19dc30
Caleb Chan <cchan@hey.com>
2026-04-13 23:30:43 -0400

    fix: remove optimistic updates for content deletion

--- src/components/ContentTable.tsx
+++ src/components/ContentTable.tsx
@@ -68,21 +68,6 @@ export function ContentTable({
 		useDisclosure(false)
 	const deleteContent = useMutation(
 		trpc.content.delete.mutationOptions({
-			onMutate: async (data, context) => {
-				const listContent = trpc.content.list.queryKey()
-				await context.client.cancelQueries({ queryKey: listContent })
-				const previousContent = context.client.getQueryData(listContent)
-				context.client.setQueryData(listContent, (old) => ({
-					...old!,
-					content: old!.content.filter((item) => !data.ids.includes(item.id)) ?? [],
-				}))
-				return { previousContent }
-			},
-			onError: (err, data, onMutateResult, context) => {
-				if (onMutateResult) {
-					context.client.setQueryData(trpc.content.list.queryKey(), onMutateResult.previousContent)
-				}
-			},
 			onSettled() {
 				queryClient.invalidateQueries({ queryKey: trpc.content.list.queryKey() })
 				queryClient.invalidateQueries({ queryKey: trpc.content.listFavorites.queryKey() })
@@ -479,9 +464,9 @@ export function ContentTable({
 					<Button
 						color="red"
 						loading={deleteContent.isPending}
-						onClick={() => {
+						onClick={async () => {
 							const idsToDelete = table.getSelectedRowModel().rows.map((r) => r.original.id)
-							deleteContent.mutate(
+							await deleteContent.mutateAsync(
 								{ ids: idsToDelete },
 								{
 									onSuccess: () => {
@@ -490,7 +475,6 @@ export function ContentTable({
 										table.options.data = table.options.data.filter(
 											(item) => !idsToDelete.includes(item.id)
 										)
-										queryClient.invalidateQueries({ queryKey: trpc.content.list.queryKey() })
 										notifications.show({
 											title: "Content deleted",
 											message: "The selected content has been deleted successfully.",
