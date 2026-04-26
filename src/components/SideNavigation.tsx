bfe3939ea635987524f32e32e9c0b73c5027d2aa
PhilBanoub <pbanoub@wpi.edu>
2026-04-26 07:14:12 -0400

    fix: biome

--- src/components/SideNavigation.tsx
+++ src/components/SideNavigation.tsx
@@ -1,5 +1,5 @@
 import { useAuth0 } from "@auth0/auth0-react"
-import { Button, Group, Menu, Stack, Text } from "@mantine/core"
+import { Button, Menu, Stack, Text } from "@mantine/core"
 import {
 	IconBolt,
 	IconBuildingBank,
