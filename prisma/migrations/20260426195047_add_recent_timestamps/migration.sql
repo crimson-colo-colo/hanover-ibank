fc00dc08c0fe8fc34c09427548e16a04ba2d8cfc
Justin Fletcher <justin.fletcher22@gmail.com>
2026-04-26 16:18:06 -0400

    feat: modify back-end schema and seed script to support recent timestamps
    Co-authored-by: Brandon Gainey <b.danielgainey07@gmail.com>

---
+++ prisma/migrations/20260426195047_add_recent_timestamps/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "RecentTimestamps" (
+    "recentlyEdited" TIMESTAMP(3) NOT NULL,
+    "recentlyViewed" TIMESTAMP(3) NOT NULL,
+    "employeeId" TEXT NOT NULL,
+    "contentId" TEXT NOT NULL,
+
+    CONSTRAINT "RecentTimestamps_pkey" PRIMARY KEY ("recentlyEdited","recentlyViewed","employeeId","contentId")
+);
+
+-- AddForeignKey
+ALTER TABLE "RecentTimestamps" ADD CONSTRAINT "RecentTimestamps_employeeId_fkey" FOREIGN KEY ("employeeId") REFERENCES "Employee"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "RecentTimestamps" ADD CONSTRAINT "RecentTimestamps_contentId_fkey" FOREIGN KEY ("contentId") REFERENCES "Content"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
