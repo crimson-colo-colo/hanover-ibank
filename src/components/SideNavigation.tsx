733bc75deb7d914853535377a90018f451cf4672
PhilBanoub <pbanoub@wpi.edu>
2026-04-27 03:26:12 -0400

    feat: side nav bar doesn't render when user is not logged, small css fixes

--- src/components/SideNavigation.tsx
+++ src/components/SideNavigation.tsx
@@ -1,13 +1,5 @@
 import { useAuth0 } from "@auth0/auth0-react"
-import {
-	ActionIcon,
-	Button,
-	Menu,
-	Stack,
-	Text,
-	Tooltip,
-	NavLink
-} from "@mantine/core"
+import { ActionIcon, Button, Group, Menu, NavLink, Stack, Text, Tooltip } from "@mantine/core"
 import {
 	IconBolt,
 	IconBuildingBank,
@@ -17,15 +9,16 @@ import {
 	IconInfoCircle,
 	IconLayoutSidebarLeftExpand,
 	IconLayoutSidebarRightExpand,
-	IconList, IconPointFilled,
+	IconList,
+	IconPointFilled,
 	IconStar,
 	IconUser,
 	IconUsers,
 } from "@tabler/icons-react"
+import { useQuery } from "@tanstack/react-query"
 import { Link, useLocation } from "@tanstack/react-router"
-import { trpc } from "@/lib/trpc.ts"
 import { Avatar } from "@/components/Avatar.tsx"
-import {useQuery} from "@tanstack/react-query";
+import { trpc } from "@/lib/trpc.ts"
 
 export type sideNavigationProps = {
 	collapsed: boolean
@@ -33,42 +26,44 @@ export type sideNavigationProps = {
 }
 
 const routeLinks = [
-	{pathName: "/dashboard", name: "Home", icon: IconHome},
-	{pathName: "/content-table", name: "Content", icon: IconList},
-	{pathName: "/favorites", name: "Favorites", icon: IconStar},
-	{pathName: "/about", name: "About", icon: IconInfoCircle},
-	{pathName: "/technology", name: "Technology", icon: IconBolt},
-	{pathName: "/admin/manage-users", name: "Employees", icon: IconUsers, admin: true},
-	{pathName: "/admin/analytics", name: "Analytics", icon: IconChartBarPopular, admin: true},
+	{ pathName: "/dashboard", name: "Home", icon: IconHome },
+	{ pathName: "/content-table", name: "Content", icon: IconList },
+	{ pathName: "/favorites", name: "Favorites", icon: IconStar },
+	{ pathName: "/about", name: "About", icon: IconInfoCircle },
+	{ pathName: "/technology", name: "Technology", icon: IconBolt },
+	{ pathName: "/admin/manage-users", name: "Employees", icon: IconUsers, admin: true },
+	{ pathName: "/admin/analytics", name: "Analytics", icon: IconChartBarPopular, admin: true },
 ]
 
-function AdminLinks({isAdmin, collapsed}: {isAdmin: boolean | undefined, collapsed: boolean},) {
+function AdminLinks({ isAdmin, collapsed }: { isAdmin: boolean | undefined; collapsed: boolean }) {
 	const location = useLocation()
 
-	const adminNavLinks = routeLinks.filter((link) => link.admin).map((link) => (
-		<Tooltip label={link.name} position="right" withArrow arrowSize={8} disabled={!collapsed}>
-			<Button
-				component={Link}
-				variant={location.pathname === link.pathName ? "light" : "subtle"}
-				to={link.pathName}
-				rightSection={!collapsed && <IconChevronRight size={20}/>}
-				justify={collapsed ? "center" : "space-between"}
-				radius={0}
-			>
-				{<link.icon />}
-				{!collapsed && <span className="p-2">{link.name}</span>}
-			</Button>
-		</Tooltip>
-	))
+	const adminNavLinks = routeLinks
+		.filter((link) => link.admin)
+		.map((link) => (
+			<Tooltip label={link.name} position="right" withArrow arrowSize={8} disabled={!collapsed}>
+				<Button
+					component={Link}
+					variant={location.pathname === link.pathName ? "light" : "subtle"}
+					to={link.pathName}
+					rightSection={!collapsed && <IconChevronRight size={20} />}
+					justify={collapsed ? "center" : "space-between"}
+					radius={0}
+				>
+					{<link.icon />}
+					{!collapsed && <span className="p-2">{link.name}</span>}
+				</Button>
+			</Tooltip>
+		))
 
 	return (
 		<>
-			{isAdmin && (
-				collapsed ? (
+			{isAdmin &&
+				(collapsed ? (
 					<div>
-						<Stack gap={0} className="items-center">
-							<div className="my-2">
-								<IconPointFilled color="gray"/>
+						<Stack gap={0}>
+							<div className="flex justify-center my-2">
+								<IconPointFilled color="gray" />
 							</div>
 							{adminNavLinks}
 						</Stack>
@@ -77,7 +72,7 @@ function AdminLinks({isAdmin, collapsed}: {isAdmin: boolean | undefined, collaps
 					<div>
 						<hr />
 						<Stack gap={0}>
-							<Text c="dimmed" className="ml-5 mb-2 text-sm">
+							<Text c="dimmed" className="ml-5 mb-2 text-xs uppercase">
 								Administration
 							</Text>
 							{adminNavLinks}
@@ -88,114 +83,90 @@ function AdminLinks({isAdmin, collapsed}: {isAdmin: boolean | undefined, collaps
 	)
 }
 
-export function SideNavigation({collapsed, toggleCollapsed} : sideNavigationProps) {
+export function SideNavigation({ collapsed, toggleCollapsed }: sideNavigationProps) {
 	const auth0 = useAuth0()
 	const isAdmin = useQuery(trpc.admin.isAdmin.queryOptions())
 	const location = useLocation()
 
-	const navLinks = routeLinks.filter((link) => !link.admin).map((link) => (
-		<Tooltip label={link.name} position="right" withArrow arrowSize={8} disabled={!collapsed}>
-			<Button
-				component={Link}
-				variant={location.pathname === link.pathName ? "light" : "subtle"}
-				to={link.pathName}
-				rightSection={!collapsed && <IconChevronRight size={20}/>}
-				justify={collapsed ? "center" : "space-between"}
-				radius={0}
-			>
-				{<link.icon/>}
-				{!collapsed && <span className="p-2">{link.name}</span>}
-			</Button>
-		</Tooltip>
-	))
+	const navLinks = routeLinks
+		.filter((link) => !link.admin)
+		.map((link) => (
+			<Tooltip label={link.name} position="right" withArrow arrowSize={8} disabled={!collapsed}>
+				<Button
+					component={Link}
+					variant={location.pathname === link.pathName ? "light" : "subtle"}
+					to={link.pathName}
+					rightSection={!collapsed && <IconChevronRight size={20} />}
+					justify={collapsed ? "center" : "space-between"}
+					radius={0}
+				>
+					{<link.icon />}
+					{!collapsed && <span className="p-2">{link.name}</span>}
+				</Button>
+			</Tooltip>
+		))
 
 	return (
 		<header className="relative h-full bg-gray-50 dark:bg-gray-900 dark:border-gray-700">
 			<div className="p-4">
-					<Link
-						to="/"
-						className={`flex items-center ${collapsed && "justify-center"} gap-2 mb-4 no-underline active:text-primary-hover`}
-					>
-						<IconBuildingBank />
-						{!collapsed && <span className="text-xl font-semibold font-display">iBank</span>}
-					</Link>
-				</div>
-				<Stack gap={0}>
-					{navLinks}
-				</Stack>
-				{<AdminLinks isAdmin={isAdmin.data} collapsed={collapsed}></AdminLinks>}
+				<Link
+					to="/"
+					className={`flex items-center ${collapsed && "justify-center"} gap-2 mb-4 no-underline active:text-primary-hover`}
+				>
+					<IconBuildingBank />
+					{!collapsed && <span className="text-xl font-semibold font-display">iBank</span>}
+				</Link>
+				<ActionIcon variant="subtle" size="md" onClick={toggleCollapsed}>
+					{collapsed ? (
+						<IconLayoutSidebarLeftExpand size={26} />
+					) : (
+						<IconLayoutSidebarRightExpand size={26} />
+					)}
+				</ActionIcon>
+				{/*<div className={collapsed ? "flex justify-center" : "flex justify-end-safe"}>
 
-			{!collapsed &&
+				</div>*/}
+			</div>
+			<Stack gap={0}>{navLinks}</Stack>
+			{<AdminLinks isAdmin={isAdmin.data} collapsed={collapsed}></AdminLinks>}
+
+			{!collapsed && (
 				<div>
 					<hr />
 					<Stack>
-						<Text c="dimmed" className="ml-5 mb-2 text-sm">
+						<Text c="dimmed" className="ml-5 mb-2 text-xs uppercase">
 							Recently Viewed
 						</Text>
 					</Stack>
 				</div>
-			}
+			)}
 
-			<div className={collapsed ? "absolute inset-x-0 bottom-0 mb-5" : "absolute inset-x-0 bottom-0 mb-5  flex items-center"}>
-				{!collapsed && <hr />}
-				{auth0.isAuthenticated && auth0.user ? (
-					<Menu trigger="click" position="top-start">
-						<Menu.Target>
-							<Button
-								justify="space-between"
-								variant="subtle"
-								color="gray"
-								className="h-12"
-								radius={0}
-							>
-								<div className="flex items-center gap-2 px-2 py-1">
-									<Avatar userId={auth0.user.sub!} h={collapsed ? 26 : 32}/>
-									{!collapsed && (
-										<div className="flex flex-col items-start">
-											<Text size="sm" fw={500}>
-												{auth0.user.name ?? auth0.user.nickname ?? auth0.user.username}
-											</Text>
-											<Text size="xs" c="gray">
-												{auth0.user.email}
-											</Text>
-										</div>
-									)}
-								</div>
-							</Button>
-						</Menu.Target>
-						<Menu.Dropdown className="shadow-sm">
-							<Menu.Item component={Link} to="/profile" leftSection={<IconUser />}>
-								Profile
-							</Menu.Item>
-							<Menu.Item
-								leftSection={<IconLayoutSidebarLeftExpand />}
-								onClick={() => {
-									auth0.logout({ logoutParams: { returnTo: window.location.origin } })
-								}}
-								color="red"
-							>
-								Sign Out
-							</Menu.Item>
-						</Menu.Dropdown>
-					</Menu>
-				) : (
-					<Button
-						onClick={() => {
-							auth0.loginWithRedirect()
-						}}
-						fullWidth
-					>
-						Login
-					</Button>
-				)}
-				<div className={collapsed ? "flex justify-center" : "flex justify-end-safe"}>
-					<ActionIcon variant="subtle" size="md" onClick={toggleCollapsed}>
-						{collapsed ? <IconLayoutSidebarLeftExpand size={26}/> :
-							<IconLayoutSidebarRightExpand size={26}/>}
-					</ActionIcon>
-				</div>
-			</div>
-
+			{auth0.isAuthenticated && auth0.user && (
+				<Button
+					component={Link}
+					to="/profile"
+					justify="left"
+					variant="subtle"
+					color="gray"
+					className={`h-14 absolute bottom-0 mb-3 ${!collapsed && "border-0 border-t border-gray-500"}`}
+					radius={0}
+					fullWidth
+				>
+					<div className="flex items-center gap-3 px-2 py-1">
+						<Avatar userId={auth0.user.sub!} w={collapsed ? "lg" : "xl"} />
+						{!collapsed && (
+							<div className="flex flex-col items-start">
+								<Text size="sm" fw={500}>
+									{auth0.user.name ?? auth0.user.nickname ?? auth0.user.username}
+								</Text>
+								<Text size="xs" c="gray">
+									{auth0.user.email}
+								</Text>
+							</div>
+						)}
+					</div>
+				</Button>
+			)}
 		</header>
 	)
 }
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,4 +1,4 @@
-import type { Auth0ContextInterface, User } from "@auth0/auth0-react"
+import { type Auth0ContextInterface, type User, useAuth0 } from "@auth0/auth0-react"
 import { AppShell, localStorageColorSchemeManager, MantineProvider } from "@mantine/core"
 import { Notifications } from "@mantine/notifications"
 import { DevSupport } from "@react-buddy/ide-toolbox"
@@ -20,7 +20,7 @@ import { queryClient } from "@/lib/trpc.ts"
 import { theme } from "@/theme.ts"
 import "../styles.css"
 import clsx from "clsx"
-import {useState} from "react";
+import { useState } from "react"
 
 interface RouterContext {
 	auth0: Auth0ContextInterface<User>
@@ -38,6 +38,7 @@ function RootComponent() {
 	const location = useLocation()
 	const isPreview = location.pathname.startsWith("/preview")
 	const [collapsed, toggleCollapsed] = useState(false)
+	const auth0 = useAuth0()
 
 	const colorSchemeManager = localStorageColorSchemeManager({
 		key: "mantine-color-scheme",
@@ -49,18 +50,26 @@ function RootComponent() {
 				<DevSupport ComponentPreviews={() => null} useInitialHook={useInitial}>
 					<AppShell
 						padding={isPreview ? 0 : "md"}
-						//header={{ height: 56 }}
-						navbar={{ width: collapsed ? "80" : "260", breakpoint: "sm" }}
+						header={{ height: 56 }}
+						navbar={{
+							width: auth0.isAuthenticated && auth0.user ? (collapsed ? "70" : "260") : "0",
+							breakpoint: "sm",
+						}}
 						//transitionDuration={1000}
 						//transitionTimingFunction="ease"
 						className={clsx(isPreview && "not-dark:bg-gray-100")}
 					>
-						{/*<AppShell.Header>
+						<AppShell.Header>
 							<Navigation />
-						</AppShell.Header>*/}
-						<AppShell.Navbar>
-							<SideNavigation collapsed={collapsed} toggleCollapsed={() => toggleCollapsed((c) => !c)} />
-						</AppShell.Navbar>
+						</AppShell.Header>
+						{auth0.isAuthenticated && auth0.user && (
+							<AppShell.Navbar>
+								<SideNavigation
+									collapsed={collapsed}
+									toggleCollapsed={() => toggleCollapsed((c) => !c)}
+								/>
+							</AppShell.Navbar>
+						)}
 						<AppShell.Main className={clsx(!isPreview && "mx-auto max-w-325")}>
 							<HeadContent />
 							<Outlet />
